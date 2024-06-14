f53ae63d1fe2e70b8a9e6e13c5d737415efb6b37
ankit <ankitbhagat2062@gmail.com>
2024-06-14 08:47:04 +0545

    Initial commit

---
+++ js/faq.js
@@ -0,0 +1,127 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const faqitems = document.querySelector('.faq-items');
+    const collapsed = document.querySelector('.collapsed');
+
+    const faqitems1 = document.querySelector('.faq-items1');
+    const collapsed1 = document.querySelector('.collapsed1');
+
+    const faqitems2 = document.querySelector('.faq-items2');
+    const collapsed2 = document.querySelector('.collapsed2');
+
+    const faqitems3 = document.querySelector('.faq-items3');
+    const collapsed3 = document.querySelector('.collapsed3');
+
+    const faqitems4 = document.querySelector('.faq-items4');
+    const collapsed4 = document.querySelector('.collapsed4');
+
+    const faqitems5 = document.querySelector('.faq-items5');
+    const collapsed5 = document.querySelector('.collapsed5');
+
+    const faqitems6 = document.querySelector('.faq-items6');
+    const collapsed6 = document.querySelector('.collapsed6');
+
+    const faqitems7 = document.querySelector('.faq-items7');
+    const collapsed7 = document.querySelector('.collapsed7');
+
+    const faqitems8 = document.querySelector('.faq-items8');
+    const collapsed8 = document.querySelector('.collapsed8');
+    
+    const faqitems9 = document.querySelector('.faq-items9');
+    const collapsed9 = document.querySelector('.collapsed9');
+
+    const faqitems10 = document.querySelector('.faq-items10');
+    const collapsed10 = document.querySelector('.collapsed10');
+
+    const faqitems11 = document.querySelector('.faq-items11');
+    const collapsed11 = document.querySelector('.collapsed11');
+
+    const faqitems12 = document.querySelector('.faq-items12');
+    const collapsed12 = document.querySelector('.collapsed12');
+
+    const faqitems13 = document.querySelector('.faq-items13');
+    const collapsed13 = document.querySelector('.collapsed13');
+
+    const faqitems14 = document.querySelector('.faq-items14');
+    const collapsed14 = document.querySelector('.collapsed14');
+
+    const faqitems15 = document.querySelector('.faq-items15');
+    const collapsed15 = document.querySelector('.collapsed15');
+
+    faqitems.addEventListener('click', () => {
+        faqitems.classList.toggle('f-ht-resp');
+         collapsed.classList.toggle('collapsed');
+    }); 
+
+    faqitems1.addEventListener('click', () => {
+        faqitems1.classList.toggle('f-ht-resp');
+         collapsed1.classList.toggle('collapsed1');
+    }); 
+
+    faqitems2.addEventListener('click', () => {
+        faqitems2.classList.toggle('f-ht-resp');
+         collapsed2.classList.toggle('collapsed2');
+    }); 
+
+    faqitems3.addEventListener('click', () => {
+        faqitems3.classList.toggle('f-ht-resp');
+         collapsed3.classList.toggle('collapsed3');
+    }); 
+    faqitems4.addEventListener('click', () => {
+        faqitems4.classList.toggle('f-ht-resp');
+         collapsed4.classList.toggle('collapsed4');
+    }); 
+
+    faqitems5.addEventListener('click', () => {
+        faqitems5.classList.toggle('f-ht-resp');
+         collapsed5.classList.toggle('collapsed5');
+    }); 
+    faqitems6.addEventListener('click', () => {
+        faqitems6.classList.toggle('f-ht-resp');
+         collapsed6.classList.toggle('collapsed6');
+    }); 
+
+    faqitems7.addEventListener('click', () => {
+        faqitems7.classList.toggle('f-ht-resp');
+         collapsed7.classList.toggle('collapsed7');
+    });
+    faqitems8.addEventListener('click', () => {
+        faqitems8.classList.toggle('f-ht-resp');
+         collapsed8.classList.toggle('collapsed8');
+    }); 
+ 
+    faqitems9.addEventListener('click', () => {
+        faqitems9.classList.toggle('f-ht-resp');
+         collapsed9.classList.toggle('collapsed9');
+    }); 
+    faqitems10.addEventListener('click', () => {
+        faqitems10.classList.toggle('f-ht-resp');
+         collapsed10.classList.toggle('collapsed10');
+    }); 
+    faqitems11.addEventListener('click', () => {
+        faqitems11.classList.toggle('f-ht-resp');
+         collapsed11.classList.toggle('collapsed11');
+    }); 
+
+    faqitems12.addEventListener('click', () => {
+        faqitems12.classList.toggle('f-ht-resp');
+         collapsed12.classList.toggle('collapsed12');
+    }); 
+    faqitems13.addEventListener('click', () => {
+        faqitems13.classList.toggle('f-ht-resp');
+         collapsed13.classList.toggle('collapsed13');
+    }); 
+
+    faqitems14.addEventListener('click', () => {
+        faqitems14.classList.toggle('f-ht-resp');
+         collapsed14.classList.toggle('collapsed14');
+    });
+    faqitems15.addEventListener('click', () => {
+        faqitems15.classList.toggle('f-ht-resp');
+         collapsed15.classList.toggle('collapsed15');
+    }); 
+});
+
+
+
+
+
